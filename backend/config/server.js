1b2b113157090cecfb18cf82e51c43afc9a414fb
Ben Moussa Chaker <chaker.info@gmail.com>
2021-11-19 16:34:23 +0100

    Create Strapi starter project

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'dc72d6ef8166ac5ae7527daa3787beaf'),
+    },
+  },
+});
